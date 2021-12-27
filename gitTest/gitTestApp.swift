efaebe6c1a505eb2a36c637e0a6febd9d5e559f2
henrique campanha <henriqueoc@msn.com>
2021-12-27 19:35:03 +0000

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by henrique campanha on 27/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
